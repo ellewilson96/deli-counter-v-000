ca6f3f0e1a0c44cd24834b89b5d849b5e0a3acaa
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 18:56:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array}"
+  end
+end
+
+def take_a_number(array)
+  array.push
+end
