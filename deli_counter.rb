12626d652a391d288732c2952444ff33deb9335f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 19:02:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     line(katz_deli)
-    puts "The line is currently: #{array.join(", ")}"
+    puts "The line is currently: #{katz_deli.join(", ")}"
   end
 end
 
