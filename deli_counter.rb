3b790ce0020affc0abfa06310740b3e7b1841833
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 18:59:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
+    array.insert(first, 1.)
     puts "The line is currently: #{array.join(", ")}"
   end
 end
@@ -12,3 +13,5 @@ end
 def take_a_number(array)
   array.push
 end
+
+insert index+1 into the same index as name 
